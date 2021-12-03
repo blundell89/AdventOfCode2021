705168d446853292397817e7774b58ceddaeddf4
Lee Blundell <lee.blundell@exizent.com>
2021-12-03 15:59:58 +0000

    Complete day 2

--- AoC.App/Day2_Dive.cs
+++ AoC.App/Day2_Dive.cs
@@ -2,7 +2,7 @@
 {
     public class Day2_Dive
     {
-        public static (int Horizontal, int Depth, int Aim) CalculatePositionDepth(string[] commands)
+        public static (int Horizontal, int Depth) CalculatePositionDepth(string[] commands)
         {
             var splitCommands = commands.Select(x =>
             {
@@ -29,7 +29,38 @@
                 }
             }
 
-            return (x, y, 0);
+            return (x, y);
+        }
+
+        public static (int Horizontal, int Depth) CalculatePositionDepthRevisited(string[] commands)
+        {
+            var splitCommands = commands.Select(x =>
+            {
+                var split = x.Split(' ');
+
+                return (Command: split[0], Num: int.Parse(split[1]));
+            }).ToArray();
+
+            int x = 0, y = 0, aim = 0;
+
+            foreach (var (command, num) in splitCommands)
+            {
+                switch (command)
+                {
+                    case "forward":
+                        x += num;
+                        y += (aim * num);
+                        break;
+                    case "down":
+                        aim += num;
+                        break;
+                    case "up":
+                        aim -= num;
+                        break;
+                }
+            }
+
+            return (x, y);
         }
     }
 }
--- AoC.Test/Day2.cs
+++ AoC.Test/Day2.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 using AoC.App;
 using FluentAssertions;
@@ -34,5 +33,32 @@ public class Day2
 
         var result = Day2_Dive.CalculatePositionDepth(commands);
         (result.Horizontal * result.Depth).Should().Be(1840243);
+    }
+
+    [Fact]
+    public void Test2_Dive_Example()
+    {
+        var commands = new[]
+        {
+            "forward 5",
+            "down 5",
+            "forward 8",
+            "up 3",
+            "down 8",
+            "forward 2"
+        };
+
+        var result = Day2_Dive.CalculatePositionDepthRevisited(commands);
+        (result.Horizontal * result.Depth).Should().Be(900);
+    }
+
+    [Fact]
+    public async Task Test2_Dive()
+    {
+        using var reader = ResourceReaderFactory.Create("2-1.txt");
+        var commands = (await reader.ReadToEndAsync()).Split(Environment.NewLine);
+
+        var result = Day2_Dive.CalculatePositionDepthRevisited(commands);
+        (result.Horizontal * result.Depth).Should().Be(1727785422);
     }
 }
